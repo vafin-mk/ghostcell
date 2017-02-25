7e1263e6b16dd32495b6505734c70c68e4457945
Marat Vafin <lastvar@gmail.com>
2017-02-25 19:42:41 +0300

    init

---
+++ src/main/java/ai/AI.java
@@ -0,0 +1,24 @@
+package ai;
+
+import java.util.Scanner;
+
+public class AI {
+  final Scanner scanner;
+
+  public AI(Scanner scanner) {
+    this.scanner = scanner;
+  }
+
+  public void start() {
+    inputWorld();
+    outputAnswer();
+  }
+
+  private void inputWorld() {
+
+  }
+
+  private void outputAnswer() {
+
+  }
+}
